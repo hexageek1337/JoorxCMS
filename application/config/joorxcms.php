3dcd79f3d03070b42ef7ff75fa0e0a1bb460fc84
Hexa Geeks <hexageek1337@gmail.com>
2017-09-16 17:28:29 +0700

    Joorx CMS V1.0

--- application/config/joorxcms.php
+++ application/config/joorxcms.php
@@ -10,10 +10,10 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 |_________________________________________________________________________|
 */
 
-$config['joorxcms_title'] = 'Joorx CMS'; // insert title your website
-$config['joorxcms_description'] = 'Berbagai Informasi Teknologi Teraktual dan Terbaru'; // insert description your website
-$config['joorxcms_keywords'] = 'teknologi,teraktual,terbaru,berita,informasi,tech,blog,seo,marketing'; // insert keywords your website
+$config['joorxcms_title'] = ''; // insert title your website
+$config['joorxcms_description'] = ''; // insert description your website
+$config['joorxcms_keywords'] = ''; // insert keywords your website
 $config['joorxcms_copyright'] = 'JoorxCMS'; // please do not change this copyright
-$config['joorxcms_author'] = 'Denny Septian'; // insert author your website
-$config['joorxcms_language'] = 'Indonesia'; // insert language your website , example : Indonesia / English
-$config['joorxcms_url'] = 'http://localhost/'; // with HTTP or HTTPs , example : http://yoursite.com/
+$config['joorxcms_author'] = ''; // insert author your website
+$config['joorxcms_language'] = ''; // insert language your website , example : Indonesia / English
+$config['joorxcms_url'] = ''; // with HTTP or HTTPs , example : http://yoursite.com/
