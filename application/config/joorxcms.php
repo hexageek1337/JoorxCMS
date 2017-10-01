a34e8c9da45ae70c52aaf41ebcc4bf339980fff5
Hexa Geeks <hexageek1337@gmail.com>
2017-10-01 19:29:36 +0700

    setting your seo

--- application/config/joorxcms.php
+++ application/config/joorxcms.php
@@ -6,14 +6,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Settings SEO -> Joorx CMS                                               |
 |                                                                         |
 |-------------------------------------------------------------------------|
-| Version 1.0                                                             |
+| Version 1.5                                                             |
 |_________________________________________________________________________|
 */
 
-$config['joorxcms_title'] = 'Joorx CMS'; // insert title your website
-$config['joorxcms_description'] = 'Get your akwdsakdaw'; // insert description your website
-$config['joorxcms_keywords'] = 'seo,blog,tech,teknologi,joorxcms'; // insert keywords your website
+$config['joorxcms_title'] = ''; // insert title your website
+$config['joorxcms_description'] = ''; // insert description your website
+$config['joorxcms_keywords'] = ''; // insert keywords your website
 $config['joorxcms_copyright'] = 'JoorxCMS'; // please do not change this copyright
-$config['joorxcms_author'] = 'Denny Septian'; // insert author your website
-$config['joorxcms_language'] = 'Indonesia'; // insert language your website , example : Indonesia / English
-$config['joorxcms_url'] = 'http://localhost/'; // with HTTP or HTTPs , example : http://yoursite.com/
+$config['joorxcms_author'] = ''; // insert author your website
+$config['joorxcms_language'] = ''; // insert language your website , example : Indonesia / English
+$config['joorxcms_url'] = ''; // with HTTP or HTTPs , example : http://yoursite.com/
