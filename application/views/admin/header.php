611ad5c74acaa5fca694eae1cf5e27c4529fc622
Denny Septian <hexageek1337@gmail.com>
2018-01-24 15:08:32 +0700

    fix header link

--- application/views/admin/header.php
+++ application/views/admin/header.php
@@ -51,8 +51,8 @@
     </div>
     <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
       <ul class="nav navbar-nav navbar-left">
-        <li><a href="/admin/berita">Berita</a></li>
-        <li><a href="/admin/category">Category</a></li>
+        <li><a href="<?php echo base_url(); ?>admin/berita">Berita</a></li>
+        <li><a href="<?php echo base_url(); ?>admin/category">Category</a></li>
       </ul>
       <ul class="nav navbar-nav navbar-right">
         <li class="dropdown">
